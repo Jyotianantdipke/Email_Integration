eb072c09a4b91aabf5f71c5bbf6467e0faf8875c
Jyoti <jyotidipke@gmail.com>
2021-09-19 08:52:34 +0530

    Email Sending Project Uploaded

---
+++ EmailSending/SendMail/views.py
@@ -0,0 +1,21 @@
+
+from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+
+# Create your views here.
+
+def sendmail(request):
+
+    if request.method=='POST':
+        email=request.POST.get('email')
+        subject='Welcome to django'
+        message= 'Hi, This is a Demo of how you can send email in django'
+        email_from= settings.EMAIL_HOST_USER
+        recipient_list= [email]
+        send_mail(subject,message,email_from,recipient_list)
+        return render(request,'SendMail/dashboard.html',{'recipient_list':recipient_list})
+    return render(request,'SendMail/sendmail.html')
+
+
+
